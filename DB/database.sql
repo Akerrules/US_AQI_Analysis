26f9c931267643069269efbc425cbb04ed26c34d
akerrules <addrido@gmail.com>
2024-04-02 15:31:51 -0400

    updated the db python file

--- DB/database.sql
+++ DB/database.sql
@@ -1,20 +1,22 @@
 CREATE TABLE Geography (
-    geography_id INTEGER PRIMARY KEY,
+    geography_id SERIAL PRIMARY KEY,
     State VARCHAR NOT NULL,
     City VARCHAR NOT NULL,
     Timezone VARCHAR NOT NULL,
     Population INTEGER NOT NULL,
     Population_Density INTEGER NOT NULL,
-    Area_Code INTEGER NOT NULL
+    Area_Code INTEGER NOT NULL,
+    CONSTRAINT unique_geo UNIQUE (State, City,Timezone,Area_Code,Population,Population_Density)
 );
 
 CREATE TABLE Date (
-    date_id INTEGER PRIMARY KEY,
+    date_id SERIAL PRIMARY KEY,
     day INTEGER NOT NULL,
-    month VARCHAR NOT NULL,
+    month INTEGER NOT NULL,
     year INTEGER NOT NULL,
     day_of_the_week VARCHAR NOT NULL,
-    season VARCHAR NOT NULL
+    season VARCHAR NOT NULL,
+    CONSTRAINT unique_date UNIQUE (day, month, year, day_of_the_week,season)
 );
 
 CREATE TABLE Pollutant (
@@ -29,14 +31,15 @@ CREATE TABLE Pollutant (
 );
 
 CREATE TABLE Station (
-    station_id INTEGER PRIMARY KEY,
+    station_id SERIAL PRIMARY KEY,
 	ID VARCHAR NOT NULL,
     Scale_Type VARCHAR NOT NULL,
-    Status VARCHAR NOT NULL
+    Status VARCHAR NOT NULL,
+    CONSTRAINT unique_station UNIQUE (ID, Scale_Type, Status)
 );
 
 CREATE TABLE Weather (
-    weather_id INTEGER PRIMARY KEY,
+    weather_id SERIAL PRIMARY KEY,
     tmax INTEGER NOT NULL,
     tmin INTEGER NOT NULL,
     prcp FLOAT NOT NULL
@@ -44,13 +47,14 @@ CREATE TABLE Weather (
 
 -- Fact Table
 CREATE TABLE AirQuality (
-    aqi_id SERIAL PRIMARY KEY,
-    geography_id INTEGER REFERENCES Geography(geography_id),
-    date_id INTEGER REFERENCES Date(date_id),
-    pollutant_id INTEGER REFERENCES Pollutant(pollutant_id),
-    station_id INTEGER REFERENCES Station(station_id),
-    weather_id INTEGER REFERENCES Weather(weather_id),
+    PRIMARY KEY (geography_id, date_id, pollutant_id, station_id, weather_id),
+    geography_id BIGINT REFERENCES Geography(geography_id),
+    date_id BIGINT REFERENCES Date(date_id),
+    pollutant_id BIGINT REFERENCES Pollutant(pollutant_id),
+    station_id BIGINT REFERENCES Station(station_id),
+    weather_id BIGINT REFERENCES Weather(weather_id),
     aqi INTEGER NOT NULL,
     aqhi INTEGER NOT NULL,
     visibility_range INTEGER NOT NULL
+    
 );
--- DB/db.py
+++ DB/db.py
@@ -1,66 +1,162 @@
 import pandas as pd
 import psycopg2
 from psycopg2 import extras
-
+import json
 
 conn = psycopg2.connect(
     dbname="datascience",
     user="postgres",
-    password="",
+    password="password",
     host="localhost",
     port="5432"
 )
 cur = conn.cursor()
 df = pd.read_csv('Data_Staging.csv')
+
+
+
+
+
 print("getting started uploaded")
 
-for index, row in df.iterrows():
-    cur.execute("""
-        INSERT INTO Geography (geography_id, City, State, Population, Population_Density, Timezone, Area_Code)
-        VALUES (%s, %s, %s, %s, %s, %s, %s)
-    """, (row['Surrogate Keys'], row['City'], row['State'], row['Population'], row['Population Density'], row['Timezone'], row['Area Code']))
-print("Geo uploaded")
+# geo_tuples = list(df[[ 'City', 'State', 'Population', 'Population Density', 'Timezone', 'Area Code']].itertuples(index=False, name=None))
+date_tuples = list(df[[ 'day', 'month', 'year', 'season', 'day_of_week']].itertuples(index=False, name=None))
+# weather_tuples = list(df[['Surrogate Keys', 'tmax', 'tmin', 'prcp']].itertuples(index=False, name=None))
+# station_tuples = list(df[['ID', 'Scale Type', 'Status']].itertuples(index=False, name=None))
+# pollutant_tuples = list(df[['Surrogate Keys', 'NO2 Mean', 'CO Mean', 'SO2 Mean', 'O3 Mean', 'PM2.5 Mean', 'PM10 Mean', 'Category']].itertuples(index=False, name=None))
+# # fact_tuple = list(df[['Surrogate Keys', 'Surrogate Keys', 'Surrogate Keys', 'Surrogate Keys', 'Surrogate Keys', 'Surrogate Keys', 'AQI', 'AQHI', 'Visibility Range']].itertuples(index=False, name=None))
+# print("tuppling done")
 
-# Insert data into the DateInfo table
-for index, row in df.iterrows():
-    cur.execute("""
-        INSERT INTO DateInfo (date_id, day, month, year, season, day_of_the_week)
-        VALUES (%s, %s, %s, %s, %s, %s)
-    """, (row['Surrogate Keys'], row['day'], row['month'], row['year'], row['season'], row['day_of_week']))
-print("Date uploaded")
 
-# Insert data into the Weather table
-for index, row in df.iterrows():
-    cur.execute("""
-        INSERT INTO Weather (weather_id, tmax, tmin, prcp)
-        VALUES (%s, %s, %s, %s)
-    """, (row['Surrogate Keys'], row['tmax'], row['tmin'], row['prcp']))
-print("weather uploaded")
 
-# Insert data into the Station table
-for index, row in df.iterrows():
-    cur.execute("""
-        INSERT INTO Station (station_id, ID, Scale_Type, Status)
-        VALUES (%s, %s, %s, %s)
-    """, (row['Surrogate Keys'], row['ID'], row['Scale Type'], row['Status']))
-print("Station uploaded")
+def upload_geo():
+    extras.execute_batch(cur,"""
+        INSERT INTO Geography (City, State, Population, Population_Density, Timezone, Area_Code)
+        VALUES ( %s, %s, %s, %s, %s, %s)
+        ON CONFLICT (City, State, Population, Population_Density, Timezone,Area_Code) DO NOTHING;
+    """, geo_tuples)
 
-# Insert data into the Pollutant table
-for index, row in df.iterrows():
-    cur.execute("""
-        INSERT INTO Pollutant (pollutant_id, NO2_Mean, CO_Mean, SO2_Mean, O3_Mean, pm25, pm10, Risk_Level)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-    """, (row['Surrogate Keys'], row['NO2 Mean'], row['CO Mean'], row['SO2 Mean'], row['O3 Mean'], row['PM2.5 Mean'], row['PM10 Mean'], row['Category']))
-print(" Pollutant uploaded")
+    print("Geo uploaded")
+    conn.commit()
 
-# Insert data into the AQI table
-for index, row in df.iterrows():
-    cur.execute("""
-        INSERT INTO AQI (aqi_id, geography_id, date_id, weather_id, station_id, pollutant_id, aqi, aqhi, visibility_range)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """, (row['Surrogate Keys'], row['Surrogate Keys'], row['Surrogate Keys'], row['Surrogate Keys'], row['Surrogate Keys'], row['Surrogate Keys'], row['AQI'], row['AQHI'], row['Visibility Range']))
 
-print("done")
+def upload_date():
+    # Insert data into the Date table
+    extras.execute_batch(cur,"""
+            INSERT INTO Date (day, month, year, season, day_of_the_week)
+            VALUES (%s, %s, %s, %s, %s)  
+            ON CONFLICT (day,month,year,season, day_of_the_week ) DO NOTHING
+    """
+    , date_tuples)
+
+    conn.commit()
+    print("Date uploaded")
+
+
+# # Insert data into the Weather table
+def upload_weather():
+    extras.execute_batch(cur,""" INSERT INTO Weather (weather_id, tmax, tmin, prcp)  VALUES (%s, %s, %s, %s)
+        """, weather_tuples)
+
+    conn.commit()
+    print("weather uploaded")
+
+
+# # Insert data into the Station table
+def upload_station():
+    extras.execute_batch(cur,"""
+            INSERT INTO Station ( ID, Scale_Type, Status)
+            VALUES ( %s, %s, %s)
+            ON CONFLICT (ID, Scale_Type, Status) DO NOTHING 
+        """, station_tuples)
+    
+    conn.commit()
+    print("Station uploaded")
+
+# # Insert data into the Pollutant table
+def upload_pollutant():
+    extras.execute_batch(cur,"""
+            INSERT INTO Pollutant (pollutant_id, NO2_Mean, CO_Mean, SO2_Mean, O3_Mean, pm25, pm10, Risk_Level)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """, pollutant_tuples)
+
+    conn.commit()
+    print(" Pollutant uploaded")
+
+# # Insert data into the AQI table
+
+def upload_fact(fact_tuple):
+    extras.execute_batch(cur,"""
+        INSERT INTO AirQuality (date_id,geography_id,station_id , pollutant_id, weather_id,visibility_range,aqi,aqhi)
+        VALUES (%s, %s, %s, %s, %s,%s,%s,%s)
+    """, fact_tuple)
+
+
+def getkeys():
+    cache_keys = {
+    "weather":{},
+    "date":{},
+    'pollutant':{},
+    'station':{},
+    'geo':{}
+
+    }
+    cur.execute("SELECT pollutant_id FROM Pollutant;")
+    for key in cur.fetchall():  # returns a tuplein the format (id,)
+        cache_keys["pollutant"][key[0]] = key[0]
+
+    
+    cur.execute("SELECT weather_id FROM Weather")
+    for key in cur.fetchall():
+        cache_keys['weather'][key[0]] = key[0]
+    
+    
+    cur.execute("SELECT station_id, ID,Scale_Type, Status FROM Station")
+    for key, ID , Scale_Type, Status in cur.fetchall():
+        cache_keys['station'][(ID, Scale_Type, Status)] = key
+    
+
+    cur.execute("SELECT geography_id, City, State, Population, Population_Density, Timezone, Area_Code FROM Geography ")
+    for key, City, State, Population, Population_Density, Timezone, Area_Code in cur.fetchall():
+        cache_keys['geo'][(City, State, Population, Population_Density, Timezone, Area_Code)] = key
+    
+
+    cur.execute("SELECT date_id, day, month, year, season, day_of_the_week FROM Date")
+    for key, day, month, year, season, day_of_the_week in cur.fetchall():
+        cache_keys['date'][(day, month, year, season, day_of_the_week)] = key
+    
+    # print(cache_keys['date'])
+    # print(cache_keys['date'][(1, '1', 1980, 'Winter', 'Tuesday')])
+    return cache_keys
+
+def prep_Fact_Data(cache_keys):
+    data_to_fact=[]
+    for index, row in df.iterrows():
+        pollutant_key = cache_keys['pollutant'][row['Surrogate Keys']]
+        weather_key = cache_keys['weather'][row['Surrogate Keys']]
+        station_key = cache_keys['station'].get((row['ID'], row['Scale Type'], row['Status']))
+        geo_key = cache_keys['geo'].get((row['City'],row['State'],row['Population'],row['Population Density'],row['Timezone'],row['Area Code']))
+        date_key = cache_keys['date'].get((row['day'], row['month'], row['year'], row['season'], row['day_of_week']))
+
+        # print([ date_key],cache_keys['date'][(row['day'], row['month'], row['year'], row['season'], row['day_of_week'])])
+        # print([pollutant_key,weather_key, station_key, geo_key, date_key])
+        if all([pollutant_key,weather_key, station_key, geo_key, date_key]):
+            data_to_fact.append((date_key,geo_key,station_key,pollutant_key,weather_key, row['Visibility Range'],row['AQI'],row['AQHI']))
+
+    return data_to_fact
+
+# upload_geo()
+# upload_pollutant()
+# upload_station()
+# upload_date()
+# upload_weather()
+
+cache_keys = getkeys()
+fact_tuples = prep_Fact_Data(cache_keys)
+upload_fact(fact_tuples)
+
 conn.commit()
+print("done")
+
 cur.close()
 conn.close()
